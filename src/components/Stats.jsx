18089afc06419ab0004c19d64788623416992acf
paruut <95574316+paruut@users.noreply.github.com>
2022-11-01 23:45:48 +0100

    Add files via upload

---
+++ src/components/Stats.jsx
@@ -0,0 +1,10 @@
+import { stats } from '../constants';
+import styles from '../style';
+
+const Stats = () => (
+    <div>Stats</div>
+  )
+
+export default Stats
+
+// https://www.youtube.com/watch?v=_oO4Qi5aVZs&t=8s 1:08:16
